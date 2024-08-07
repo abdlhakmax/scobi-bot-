206c0610ff8a9f29de806de8b75a17c357c564ec
GX <aymentz04@gmail.com>
2024-08-07 16:16:34 +0100

    🧿 ＥＬＶＥＮ － ＢＯＴ 🧿

---
+++ plugins/tools-pairphone.js
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+
+  if (!text) {
+
+    throw `اكتب الرقم بعد الخاصية\n\nمثال:\n.${usedPrefix + command} 212660131536`;
+
+  }
+
+  try {
+
+    conn.sendPresenceUpdate('composing', m.chat);
+
+    const BK9api = `https://bk9.fun/tools/PairFile?q=${encodeURIComponent(text)}`;
+
+    const BK99 = await fetch(BK9api);
+
+    const BK8 = await BK99.json();
+
+    if (BK8.status && BK8.BK9) {
+
+      const respuestaAPI = BK8.BK9;
+
+      conn.reply(m.chat, respuestaAPI, m);
+
+    } else {
+
+      throw "حدث خطأ أثناء معالجة طلبك.";
+
+    }
+
+  } catch (error) {
+
+    throw "حدث خطأ أثناء معالجة طلبك.";
+
+  }
+
+};
+
+handler.command = /^(pair)$/i;
+
+handler.help = ['pair'];
+
+handler.tags = ['tools'];
+
+export default handler;
