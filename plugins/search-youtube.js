169073f1627d91be83318e4162d42152190cce92
GX <aymentz04@gmail.com>
2024-08-08 02:06:09 +0100

    🧿 ＥＬＶＥＮ － ＢＯＴ 🧿

--- plugins/search-youtube.js
+++ plugins/search-youtube.js
@@ -1,22 +1,40 @@
-import yts from 'yt-search';
-import fs from 'fs';
-const handler = async (m, {conn, text}) => {
-  if (!text) throw ' _ضع عنوان الفيديو الذي تريد البحث عنه بعد الخاص>
-  const results = await yts(text);
-  const tes = results.all;
-  const teks = results.all.map((v) => {
-    switch (v.type) {
-      case 'video': return `
-° *_${v.title}_*
-↳   *_الرابط :_* ${v.url}
-↳ 📍 *_تاريخ النشر :_* ${v.timestamp}
-↳   *_S :_* ${v.ago}
-↳ 👁 *_المشاهدات:_* ${v.views}`;
-    }
-  }).filter((v) => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
-  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
-};
-handler.help = ['ytsearch *<text>*'];
-handler.tags = ['search'];
-handler.command = ['ytsearch', 'yts'];
-export default handler;
+import yts from 'yt-search'
+
+var handler = async (m, { text, conn, args, command, usedPrefix }) => {
+
+if (!text) return conn.reply(m.chat, `🍟 *يرجى اضافة عنوان الفيديو الذي تبحث عنه*\n\nمثال:\n ${usedPrefix + command} Kouz1 - Drari`, m, )
+
+conn.reply(m.chat, wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+
+let results = await yts(text)
+let tes = results.all
+let teks = results.all.map(v => {
+switch (v.type) {
+case 'video': return `🍟 *العنوان:* 
+» ${v.title}
+
+🔗 *الرابط:* 
+» ${v.url}
+
+🕝 *الوقت:*
+» ${v.timestamp}
+
+🚩 *تاريخ النشر:* 
+» ${v.ago}
+
+👀 *المشاهدات:* 
+» ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+
+conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
+
+}
+handler.help = ['ytsearch']
+handler.tags = ['search']
+handler.command = ['playlist', 'ytbuscar', 'yts', 'ytsearch']
+
+export default handler
