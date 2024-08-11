892ffc140ab796006267e6fbb8ca3d914d32c57a
GX <aymentz04@gmail.com>
2024-08-11 19:20:23 +0100

    🧿 ＥＬＶＥＮ － ＢＯＴ 🧿

--- plugins/dl-youtube-video.js
+++ plugins/dl-youtube-video.js
@@ -1,6 +1,8 @@
-const handler = async (m, { conn }) => {
+let handler = async (m, { args, conn }) => { 
+if (!args[0]) {
+return conn.reply(m.chat, '🍟 *يرجى ادخال الرابط بعد الخاصية*', m, rcanal)}
   try {
-      const videoLink = "https://api.bk9.site/Islam/quranvid";
+      const videoLink = "https://api.bk9.site/download/youtube";
       const videoFileName = "video.mp4";
       await conn.sendMessage(m.chat, { video: { url: videoLink }, fileName: videoFileName, mimetype: 'video/mp4', caption: 'bk9' }, { quoted: m });
   } catch (error) {
